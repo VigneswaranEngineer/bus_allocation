64aefefb38fc6ec123e16565c406d200b7720854
VigneswaranEngineer <vigneswaranengineer3063@gmail.com>
2023-08-28 23:05:12 +0530

    added html workplace

--- bus_allocation/settings.py
+++ bus_allocation/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'bus_allocation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
